d588869a6ab670efaeedccd85f5879f2627304d9
MacawHerd <90171037+MacawHerd@users.noreply.github.com>
2021-09-11 12:44:53 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ const Home: NextPage = () => {
     let network = await provider.getNetwork();
     await provider.send("eth_requestAccounts", []);
 
-    if (network.chainId != 4002) {
+    if (network.chainId != 250) {
       window.alert("Switch to Fantom to continue");
       setContextState({ ...contextState, isFantom: false, isConnected: true });
       return;
