f49a2cb2488728e399fa79659c87fdddb6a37da2
MacawHerd <90171037+MacawHerd@users.noreply.github.com>
2021-09-18 02:46:59 -0500

    Update Roadmap.tsx

--- components/pages/home/Roadmap.tsx
+++ components/pages/home/Roadmap.tsx
@@ -7,7 +7,7 @@ const roadMapData = [
   },
   {
     amt: 25,
-    info: "Charity wallet filled with 21,000 FTM. All the donations will be going to SaveTheChildren Charity. Our mission with Macaw Herd is to build a community, all while supporting charities that help children across the world.",
+    info: "Charity wallet filled with 21,000 FTM. All the donations will be going to organizations supporting victims of war or saving children. Our mission with Macaw Herd is to build a community, all while supporting charities that help children across the world.",
   },
   {
     amt: 75,
@@ -15,7 +15,7 @@ const roadMapData = [
   },
   {
     amt: 100,
-    info: "Charity wallet filled with 42,000 FTM. All the donations will be sent to SaveTheChildren charity.",
+    info: "Charity wallet filled with 42,000 FTM. All the donations will be sent to organizations supporting victims of war or saving children.",
   },
 ];
 
